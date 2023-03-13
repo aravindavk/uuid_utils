ac64854524c50812828bcd1a4e33b37a495722f2
Aravinda Vishwanathapura <mail@aravindavk.in>
2023-03-13 19:57:57 +0530

    Added default UUID namespaces for uuid3 and uuid5
    Also added doctext to all functions
    
    Signed-off-by: Aravinda Vishwanathapura <mail@aravindavk.in>

--- src/uuid_utils.cr
+++ src/uuid_utils.cr
@@ -2,21 +2,48 @@ require "digest/sha1"
 require "digest/md5"
 require "uuid"
 
-class NotImplemented < Exception
-end
+# Refer: https://www.ietf.org/rfc/rfc4122.txt
 
 struct UUID
-  def self.uuid1
-    raise NotImplemented.new "UUID v1 not implemented"
+  # Default namespaces
+  enum Namespace
+    # NameSpace_DNS
+    DNS
+
+    # NameSpace_URL
+    URL
+
+    # NameSpace_OID
+    OID
+
+    # NameSpace_X500
+    X500
   end
 
-  def self.uuid2
-    raise NotImplemented.new "UUID v2 not implemented"
+  private def self.namespace(value : Namespace | UUID)
+    case value
+    in Namespace::DNS  then UUID.new "6ba7b810-9dad-11d1-80b4-00c04fd430c8"
+    in Namespace::URL  then UUID.new "6ba7b811-9dad-11d1-80b4-00c04fd430c8"
+    in Namespace::OID  then UUID.new "6ba7b812-9dad-11d1-80b4-00c04fd430c8"
+    in Namespace::X500 then UUID.new "6ba7b814-9dad-11d1-80b4-00c04fd430c8"
+    in UUID            then value
+    end
   end
 
-  def self.uuid3(namespace : UUID, value)
+  # Generate a UUID from the MD5 hash of a namespace UUID and a name.
+  #
+  # Example:
+  # ```
+  # require "uuid_utils"
+  #
+  # namespace = UUID.new("68ab717d-d847-4277-be1a-303be8ac3d97")
+  # msg = "Hello World!"
+  # puts UUID.uuid3(namespace, msg) # => 33056d85-5061-31c8-9656-df07f3eec397
+  # ```
+  def self.uuid3(ns : UUID | Namespace, value)
+    ns = namespace(ns)
     hash = Digest::MD5.digest do |ctx|
-      ctx.update namespace.bytes
+      ctx.update ns.bytes
       ctx.update value
     end
     UUID.new(
@@ -26,13 +53,32 @@ struct UUID
     )
   end
 
+  # Generate a random UUID.
+  #
+  # Example:
+  # ```
+  # require "uuid_utils"
+  #
+  # puts UUID.uuid4 # => 39e54402-44c6-42c6-be52-4bb9ab18a5fa
+  # ```
   def self.uuid4
     UUID.random
   end
 
-  def self.uuid5(namespace : UUID, value)
+  # Generate a UUID from the SHA-1 hash of a namespace UUID and a name.
+  #
+  # Example:
+  # ```
+  # require "uuid_utils"
+  #
+  # namespace = UUID.new("68ab717d-d847-4277-be1a-303be8ac3d97")
+  # msg = "Hello World!"
+  # puts UUID.uuid5(namespace, msg) # => 562ed049-5b55-582d-ba51-812a786927ca
+  # ```
+  def self.uuid5(ns : UUID | Namespace, value)
+    ns = namespace(ns)
     hash = Digest::SHA1.digest do |ctx|
-      ctx.update namespace.bytes
+      ctx.update ns.bytes
       ctx.update value
     end
     UUID.new(
